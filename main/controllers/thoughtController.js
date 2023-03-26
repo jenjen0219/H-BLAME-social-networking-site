6b35ee1561a9ff9815a34d77757f74204fa675a0
[Jen ny] <[jengutierrez.1210@gmail.com]>
2023-03-26 14:19:45 -0700

    need to push what i have in order to get help from askbcs assitant

---
+++ main/controllers/thoughtController.js
@@ -0,0 +1,47 @@
+
+//this is what includes the user and thought models
+const { Thought, Thought } = require('../models');
+
+
+module.exports = {
+    // Get all thoughts
+    getThoughts(req, res) {
+        Thought.find()
+            .then((thoughts) => res.json(thoughts))
+            .catch((err) => res.status(500).json(err));
+    },
+    // Get a single user
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+            .select('-__v')
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought with that ID' })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    // create a new user
+    createUser(req, res) {
+        User.create(req.body)
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
+    },
+    // Update a user
+    updateUser(req, res) {
+            User.findOneAndUpdate(req, res) 
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
+    },
+    // Delete a user and associated thoughts
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with that ID' })
+                    : Thought.deleteMany({ _id: { $in: user.Thought } })
+            )
+            .then(() => res.json({ message: 'User and associated apps deleted!' }))
+            .catch((err) => res.status(500).json(err));
+    },
+};
