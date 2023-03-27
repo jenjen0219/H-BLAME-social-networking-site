ecf2b8918d9d4d59ad9f5a4f3aa8d539837bd263
[Jen ny] <[jengutierrez.1210@gmail.com]>
2023-03-26 19:24:33 -0700

    completed the thoughtController.js

--- main/controllers/thoughtController.js
+++ main/controllers/thoughtController.js
@@ -1,38 +1,63 @@
 
-//this is what includes the thought and thought models
-const { Thought, Thought } = require('../models');
+//this is what includes the thought and user models
+const { Thought, User } = require('../models');
 
 
 module.exports = {
+
     // Get all thoughts
-    getThoughts(req, res) {
+    getAllThoughts(req, res) {
         Thought.find()
             .then((thoughts) => res.json(thoughts))
             .catch((err) => res.status(500).json(err));
     },
+
     // Get a single thought
     getSingleThought(req, res) {
         Thought.findOne({ _id: req.params.thoughtId })
+            //this is excluding the __v field from the response because it isn't relevant in this instance due to the fact that we are not concerned with versioning
             .select('-__v')
             .then((thought) =>
                 !thought
-                    ? res.status(404).json({ message: 'No thought with that ID' })
-                    : res.json(thought)
+                    ? res.status(404).json({ message: 'No thought with that ID' }) : res.json(thought)
             )
             .catch((err) => res.status(500).json(err));
     },
-    // create a new thought
+
+    // create a new thought and add it to the user's thoughts array field
     createThought(req, res) {
         Thought.create(req.body)
-            .then((thought) => res.json(thought))
+            .then((thought) => {
+                return User.findOneAndUpdate(
+                    { _id: req.params.userId },
+                    //$push is an operator within MongoDB update query that allows you to add a new iten into an array without overwriting the existing array
+                    { $push: { thoughts: thought._id } },
+                    //this is passed onto the findOneAndUpdate method, which will return the updated document after the update has been applied
+                    { new: true }
+                );
+            })
+            .then((user) => {
+                if (!user) {
+                    return res.status(404).json({ message: 'No user with this ID' });
+                }
+                res.json(user);
+            })
             .catch((err) => res.status(500).json(err));
+
+
     },
+
     // Update a thought
     updateThought(req, res) {
-            Thought.findOneAndUpdate(req, res) 
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            req.body,
+            { new: true }
+        )
             .then((thought) => res.json(thought))
             .catch((err) => res.status(500).json(err));
     },
+
     // Delete a single thought 
     deleteSingleThought(req, res) {
         Thought.findOneAndDelete({ _id: req.params.thoughtId })
@@ -41,3 +66,4 @@ module.exports = {
     },
 
 };
+
